6f32068c528e920e36a3bc6ab4432829e7c650b9
Leksarus <kamil.nawrocki5@gmail.com>
2017-01-15 20:11:07 +0100

    Style about page and link to resources.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,8 +13,17 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to About page.</p>	
+			<h1 className="text-center">About</h1>
+			<p>This is a weather application build on React. I have biud this for the Complete React Web App Developer Course.</p>	
+			<p>Here are some of the tools I used</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a>
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a>
+				</li>
+			</ul>
 		</div>
 	)
 }
