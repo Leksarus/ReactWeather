397f1df1dc8f4360795bbc930c8fbf63299f0337
Leksarus <kamil.nawrocki5@gmail.com>
2017-01-16 23:34:34 +0100

    Plug search bar.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
 	onSearch: function(e) {
 		e.preventDefault();
-		alert('Not et wired up!');
+
+		var location = this.refs.searchInput.value;
+		var encodedLocation = encodeURIComponent(location);
+
+		if (location.length > 0) {
+			this.refs.searchInput.value = '';
+			window.location.hash = '#/?location=' + encodedLocation;
+		}
 	},
 
 	render: function() {
@@ -28,10 +35,10 @@ var Nav = React.createClass({
 					<form onSubmit={this.onSearch}>
 						<ul className="menu">
 							<li>
-								<input type="search" placeholder="Search weather by city" />
+								<input type="search" ref="searchInput" placeholder="Search weather by city" />
 							</li>
 							<li>
-								<input type="submit" className="button" value="Get Weather" />
+								<input type="submit" ref="searchButton" className="button" value="Get Weather" />
 							</li>
 						</ul>
 					</form>
